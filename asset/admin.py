369a37e008e8a4d81b954fc3c1a39a6c97d1ed40
zhao jiankai <jk.zhaocoder@gmail.com>
2018-01-11 20:20:03 +0800

    将表交给admin管理

--- asset/admin.py
+++ asset/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from asset import models
+admin.site.register(models.UrlGroup)
+admin.site.register(models.UrlInfor)
+admin.site.register(models.User)
+admin.site.register(models.Host)
+admin.site.register(models.Business)
+admin.site.register(models.EngineRoom)
+admin.site.register(models.Software)
